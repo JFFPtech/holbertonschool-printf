8b8a5fbe1753fc4c9da8a93a785ee7788292b5f9
JFFPtech <arrozconsalchichas1229@gmail.com>
2023-11-26 17:48:26 -0400

    compiled

--- main.h
+++ main.h
@@ -23,7 +23,7 @@ int print_d(va_list args);
 int print_i(va_list args);
 int _putchar(char c);
 int print_percent(va_list args);
-int (*get_func(char x))(va_list args);
+int (*get_func(char specifier))(va_list args);
 
 #endif
 
