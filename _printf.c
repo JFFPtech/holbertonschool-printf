225099fc6ba63bf440844141a404ca91d7a5e3a0
JFFPtech <arrozconsalchichas1229@gmail.com>
2023-11-26 17:15:32 -0400

    added changes to printf.c

--- _printf.c
+++ _printf.c
@@ -8,46 +8,35 @@
  * @format: string with format specifier
  * Return: number of characters printed
  */
-int _printf(const char *format, ...)
-{
-	if (format != NULL)
-	{
-		int count = 0, i;
-		int (*m)(va_list);
-		va_list args;
 
-		va_start(args, format);
-		i = 0;
-		if (format[0] == '%' && format[1] == '\0')
-			return (-1);
-		while (format != NULL && format[i] != '\0')
-		{
-			if (format[i] == '%')
-			{
-				if (format[i + 1] == '%')
-				{
-					count += _putchar(format[i]);
-					i += 2;
-				}
-				else
-				{
-					m = get_func(format[i + 1]);
-					if (m)
-						count += m(args);
-					else
-						count = _putchar(format[i]) + _putchar(format[i + 1]);
-					i += 2;
-				}
-			}
-			else
-			{
-				count += _putchar(format[i]);
-				i++;
-			}
-		}
-		va_end(args);
-		return (count);
-	}
-	return (-1);
+int _printf(const char *format, ...) {
+    if (format == NULL) {
+        return -1;
+    }
+
+    int count = 0;
+    va_list args;
+    va_start(args, format);
+
+    for (int i = 0; format[i] != '\0'; i++) {
+        if (format[i] == '%') {
+            if (format[i + 1] == '%') {
+                count += _putchar('%');
+                i++;
+            } else {
+                int (*m)(va_list) = get_func(format[i + 1]);
+                if (m) {
+                    count += m(args);
+                } else {
+                    count += _putchar(format[i]) + _putchar(format[i + 1]);
+                }
+                i++;
+            }
+        } else {
+            count += _putchar(format[i]);
+        }
+    }
+
+    va_end(args);
+    return count;
 }
-
