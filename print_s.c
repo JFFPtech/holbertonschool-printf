794aa0e6867d9c2264300b4e89b9d093b19312cd
JFFPtech <arrozconsalchichas1229@gmail.com>
2023-11-26 12:56:46 -0400

    added files for first problem

---
+++ print_s.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_s - prints a string
+ * @args: string  argument
+ * Return: number of characters
+ */
+int print_s(va_list args)
+{
+	int i, count = 0;
+	char *str;
+
+	i = 0;
+	str = va_arg(args, char*);
+	if (str == NULL)
+		str = "(null)";
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+		count++;
+	}
+	return (count);
+}
+
