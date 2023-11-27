be77bbbebe35e50e962168e67de4040475acbbc5
JFFPtech <arrozconsalchichas1229@gmail.com>
2023-11-26 20:46:39 -0400

    edited main.h

--- main.h
+++ main.h
@@ -6,22 +6,23 @@
 
 /**
  * struct specifier - struct specifier
- * @valid: the valid character.
- * @f: the functions associated.
+ * @valid: the valid character
+ * @f: function
  *
  */
+
 typedef struct specifier
 {
 char *valid;
 int (*f)(va_list);
 } spec;
 
+int _putchar(char c);
 int _printf(const char *format, ...);
 int print_c(va_list args);
 int print_s(va_list args);
 int print_d(va_list args);
 int print_i(va_list args);
-int _putchar(char c);
 int print_percent(va_list args);
 int (*get_func(char specifier))(va_list args);
 
